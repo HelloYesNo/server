352f27b05726d9b7a75f338f91884df33ee9edda
HelloYesGo <helloyesgo@20014c4e1f8c0000f193c2eb487624d3.dsl.pool.telekom.hu>
2025-12-10 16:35:00 +0100

    Fix Coolify install URL

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-set -ouex pipefail
+set -euo pipefail
 
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repable/index.html&protocol=https&redirect=1
 
 # Install Coolify
-curl -fsSL https://github.com/HelloYesNo/coolify/tree/v4.x/scripts/install.sh | sudo bash
+curl -fsSL https://raw.githubusercontent.com/HelloYesNo/coolify/v4.x/scripts/install.sh | bash
 
 ### Create install script
 cat > /usr/bin/coolify-start << 'EOF'
@@ -18,7 +18,7 @@ cat > /usr/bin/coolify-start << 'EOF'
 set -e
 
 cd /data/coolify/source
-docker compose up -d
+docker-compose up -d
 
 EOF
 
