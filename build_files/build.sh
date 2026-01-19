2ba1c2cc9573b90947e583812cfc3135176a4b7f
HelloYesNo <zone.rz.red@gmail.com>
2026-01-19 22:25:28 +0100

    update build

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,11 +6,39 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+#
+dnf5 remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
 
 # this installs a package from fedora repos
+dnf5 config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
+
+dnf5 install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 dnf5 copr enable swayfx/swayfx -y
 dnf5 copr enable solopasha/hyprland -y
 dnf5 install -y nvim gh swayfx sway-config-fedora hyprland firefox
+
+
+mkdir -p /data/coolify/{source,ssh,applications,databases,backups,services,proxy,webhooks-during-maintenance}
+mkdir -p /data/coolify/ssh/{keys,mux}
+mkdir -p /data/coolify/proxy/dynamic
+
+curl -fsSL https://cdn.coollabs.io/coolify/docker-compose.yml -o /data/coolify/source/docker-compose.yml
+curl -fsSL https://cdn.coollabs.io/coolify/docker-compose.prod.yml -o /data/coolify/source/docker-compose.prod.yml
+curl -fsSL https://cdn.coollabs.io/coolify/.env.production -o /data/coolify/source/.env
+curl -fsSL https://cdn.coollabs.io/coolify/upgrade.sh -o /data/coolify/source/upgrade.sh
+
+chown -R 9999:root /data/coolify
+chmod -R 700 /data/coolify
 
 #### Example for enabling a System Unit File
 
