b1abefd0732005e060b09572b8fbe6a7642f15cc
HelloYesNo <38365632+HelloYesNo@users.noreply.github.com>
2026-01-19 16:21:50 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,7 +8,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y lspci
+dnf5 install -y lspci nmcli git gh curl wget nvim
 
 #### Example for enabling a System Unit File
 
